40562a4196a7a0b8e4371fc859508c90a6f55557
Kijtapart Nawattranakul <scc385@gmail.com>
2025-02-02 22:22:39 +0700

    feat: enable adding a new expense

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,41 @@ import {
     TableCell,
     TableBody,
     Table,
-    Checkbox,
     Card,
     CardContent,
-    CardHeader
+    CardHeader,
+    ModalHeader,
+    ModalContent,
+    Button,
+    Modal,
+    Checkbox,
+    FormField,
+    Form,
+    Input,
+    Label,
 } from 'semantic-ui-react';
 
-import {Button} from 'semantic-ui-react'
 
 import './App.css'
 import {Expense, Category} from "./models/expense.js";
 
-const initialExpenses = [
-    new Expense("Whiskers Cat food", Category.Food.name, 10),
-    new Expense("Self cleaning cat Litter box", Category.Furniture.name, 500),
-    new Expense("Diamond Cat collar", Category.Accessory.name, 1000),
-];
+const initialExpenses = [new Expense("Whiskers Cat food", Category.Food.name, 10), new Expense("Self cleaning cat Litter box", Category.Furniture.name, 500), new Expense("Diamond Cat collar", Category.Accessory.name, 1000),];
 
 function App() {
     const [expenses, setExpenses] = useState(initialExpenses);
+
+    // add expense logic
+    const [showModal, setShowModal] = useState(false)
+
+    // form to add a new expense
+    const baseExpense = {
+        item: '', category: Category.Food.name, amount: '',
+    };
+    const [newExpense, setNewExpense] = useState(baseExpense);
+    const handleFormInputChange = (name, value) => setNewExpense({...newExpense, [name]: value})
+    const resetExpenseForm = function () {
+        setNewExpense(baseExpense);
+    };
 
     // expensesToDelete = [expenseName1, expenseName2]
     // this list contains the list of expenses to be removed. We can delete this by name
@@ -33,20 +49,56 @@ function App() {
         console.log('paul: handleExpenseCheck: ', expenseName);
         if (checked) {
             // make sure this expense to delete is store here
-            setExpensesToDelete([
-                ...expensesToDelete,
-                expenseName
-            ])
+            setExpensesToDelete([...expensesToDelete, expenseName])
         } else {
             // make sure this expense is marked to be deleted
-            setExpensesToDelete(
-                expensesToDelete.filter(currentName => currentName !== expenseName)
-            );
+            setExpensesToDelete(expensesToDelete.filter(currentName => currentName !== expenseName));
         }
-        console.log('expensesToDelete: ', expensesToDelete)
     };
     return (<>
         <div className="h-full w-screen">
+            <Modal
+                size="small"
+                open={showModal}
+                onClose={() => {
+                    resetExpenseForm();
+                    setShowModal(false);
+                }}
+                onOpen={() => setShowModal(true)}
+            >
+                <ModalHeader>Add a new expense</ModalHeader>
+                <ModalContent>
+                    <Form onSubmit={() => {
+                        const expenseToAdd = new Expense(newExpense.item, newExpense.category, newExpense.amount);
+                        setExpenses([...expenses, expenseToAdd]);
+                        resetExpenseForm();
+                        setShowModal(false);
+                    }}>
+                        <FormField>
+                            <label>Item</label>
+                            <input placeholder='Item Name' value={newExpense.item} onChange={e => {
+                                handleFormInputChange('item', e.target.value)
+                            }}/>
+                        </FormField>
+                        <FormField label='Categroy' control='select' value={newExpense.category} onChange={e => {
+                            handleFormInputChange('category', e.target.value)
+                        }}>
+                            {Object.keys(Category).map(name => (<option key={name} value={name}>{name}</option>))}
+                        </FormField>
+                        <FormField>
+                            <label>Amount</label>
+                            <Input labelPosition='right' type='text' placeholder='Item amount'>
+                                <Label basic>$</Label>
+                                <input value={newExpense.amount} onChange={e => {
+                                    handleFormInputChange('amount', e.target.value)
+                                }}/>
+                                <Label>.00</Label>
+                            </Input>
+                        </FormField>
+                        <Button type='submit'>Submit</Button>
+                    </Form>
+                </ModalContent>
+            </Modal>
             <Card className="!w-4xl">
                 <CardContent>
                     <CardHeader>
@@ -55,12 +107,22 @@ function App() {
                         </h1>
                     </CardHeader>
                     <div className="flex">
-                        <Button color='blue'>Add Expense</Button>
+                        <Button color='blue' onClick={() => setShowModal(true)}>Add Expense</Button>
                         <Button color='red'>Delete Expense</Button>
                     </div>
                     <div>
-                        RIP:
-                        {expensesToDelete}
+                        <div>
+                            RIP:
+                            {expensesToDelete}
+                        </div>
+                        <div>
+                            Debug
+                            <ul>
+                                <li>{newExpense.item}</li>
+                                <li>{newExpense.amount}</li>
+                                <li>{newExpense.category}</li>
+                            </ul>
+                        </div>
                     </div>
                     <div>
                         <Table size='small'>
@@ -90,7 +152,7 @@ function App() {
                 </CardContent>
             </Card>
         </div>
-    </>)
+    </>);
 }
 
 export default App
